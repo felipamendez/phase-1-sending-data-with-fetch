3232de58c8094a8f9089a8cf0b8d926cf1e8276f
Felipa Mendez <felipalouise@gmail.com>
2022-01-23 23:54:12 -0600

    commit tree 7e97abe077558841fef7c41dce50b0e97b1e1664

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+
+function submitData(userName, userEmail) {
+    const formData = {
+        name: userName, 
+        email: userEmail,
+    }
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(formData),
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(rsp => rsp.json())
+        .then(object => renderUser(object))
+        .catch(error => {
+            alert("Unauthorized Access")
+            renderError(error.message)
+        })  
+}
+
+
+function renderUser (userObject) {
+    let body = document.querySelector('body')
+    body.innerHTML = userObject.id
+}
+
+function renderError (errorMessage) {
+    let body = document.querySelector('body')
+    body.innerHTML = errorMessage
+}
+
+submitData("Steve", "steve@steve.com") 
+
+submitData("Sam", "sam@sam.com")
+
+submitData("Jim", "jim@jim.com")
